dfb48bee8ab921a6d13883af37b9f6621bad5524
munhoz <munhozfontana@gmail.com>
2022-11-27 09:33:44 -0300

    style: splay page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:challenge_marvel_studio/src/core/injection.dart';
-import 'package:challenge_marvel_studio/src/features/home/presenter/page/movie_page.dart';
-import 'package:challenge_marvel_studio/src/features/home/presenter/stores/movie_cubit.dart';
+import 'package:challenge_marvel_studio/src/features/splash/presenter/page/splash_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   setup();
@@ -19,10 +17,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: BlocProvider(
-        create: (_) => MovieCubit(iListAllMoviesCase: inject()),
-        child: const MoviePage(),
-      ),
+      home: const SplashPage(),
+      // home: BlocProvider(
+      //   create: (_) => MovieCubit(iListAllMoviesCase: inject()),
+      //   child: const MoviePage(),
+      // ),
     );
   }
 }
---
+++ lib/src/features/splash/presenter/page/splash_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SvgPicture.asset(
+          'images/logo.svg',
+          semanticsLabel: 'Acme Logo',
+          width: size.width * .42,
+        ),
+      ),
+    );
+  }
+}
